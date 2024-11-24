ba1c53436fd30aba1f676791cd140590e12e486a
Georg Ledermann <georg@ledermann.dev>
2024-11-24 18:22:27 +0100

    Add optional "essential" mode

--- spec/lib/shelly_pull_spec.rb
+++ spec/lib/shelly_pull_spec.rb
@@ -31,5 +31,77 @@ describe ShellyPull do
         expect(queue.length).to eq(0)
       end
     end
+
+    context 'when essential mode' do
+      let(:config) { Config.from_env(influx_mode: :essential) }
+
+      it 'queues record if power is non-zero' do
+        expect do
+          record1 = SolectrusRecord.new(id: 1, time: 1, payload: { power: 1 })
+          allow(config.adapter).to receive(:solectrus_record).and_return(record1)
+          shelly_pull.next
+        end.to change(queue, :length).by(1) # Because it's non-zero
+
+        expect do
+          record2 = SolectrusRecord.new(id: 2, time: 2, payload: { power: 2 })
+          allow(config.adapter).to receive(:solectrus_record).and_return(record2)
+          shelly_pull.next
+        end.to change(queue, :length).by(1) # Because it's non-zero
+      end
+
+      it 'queues record if power just changed to zero' do
+        expect do
+          record1 = SolectrusRecord.new(id: 3, time: 3, payload: { power: 1 })
+          allow(config.adapter).to receive(:solectrus_record).and_return(record1)
+          shelly_pull.next
+        end.to change(queue, :length).by(1) # Because it's non-zero
+
+        expect do
+          record2 = SolectrusRecord.new(id: 4, time: 4, payload: { power: 0 })
+          allow(config.adapter).to receive(:solectrus_record).and_return(record2)
+          shelly_pull.next
+        end.to change(queue, :length).by(1) # Because power just changed to zero
+      end
+
+      it 'does not queue record if power is zero' do
+        expect do
+          record1 = SolectrusRecord.new(id: 5, time: 5, payload: { power: 0 })
+          allow(config.adapter).to receive(:solectrus_record).and_return(record1)
+          shelly_pull.next
+        end.to change(queue, :length) # Because it's the first record
+
+        expect do
+          record2 = SolectrusRecord.new(id: 6, time: 6, payload: { power: 0 })
+          allow(config.adapter).to receive(:solectrus_record).and_return(record2)
+          shelly_pull.next
+        end.not_to change(queue, :length) # Because power is still zero
+      end
+
+      it 'does queue last_record before non-zero' do # rubocop:disable RSpec/MultipleExpectations
+        expect do
+          record1 = SolectrusRecord.new(id: 7, time: 7, payload: { power: 0 })
+          allow(config.adapter).to receive(:solectrus_record).and_return(record1)
+          shelly_pull.next
+        end.to change(queue, :length).by(1) # Because it's the first record
+
+        expect do
+          record2 = SolectrusRecord.new(id: 8, time: 8, payload: { power: 0 })
+          allow(config.adapter).to receive(:solectrus_record).and_return(record2)
+          shelly_pull.next
+        end.not_to change(queue, :length) # Because power is zero
+
+        expect do
+          record3 = SolectrusRecord.new(id: 9, time: 9, payload: { power: 0 })
+          allow(config.adapter).to receive(:solectrus_record).and_return(record3)
+          shelly_pull.next
+        end.not_to(change(queue, :length)) # Because power is still zero
+
+        expect do
+          record4 = SolectrusRecord.new(id: 10, time: 9, payload: { power: 1 })
+          allow(config.adapter).to receive(:solectrus_record).and_return(record4)
+          shelly_pull.next
+        end.to change(queue, :length).by(2) # Because power is non-zero and last_record power was zero
+      end
+    end
   end
 end
